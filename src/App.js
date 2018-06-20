8c1934cb002a5b180e30644021fda7eb6a54dd11
vlaunyc <vlau.nyc@gmail.com>
2018-06-20 02:39:41 -0400

    added test data to represent hackernews API json object. rendered data to DOM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+// https://github.com/HackerNews/API
+// by	  The username of the item's author.
+// descendants	 In the case of stories or polls, the total comment count.
+// id	  The item's unique id.
+// kids	  The ids of the item's comments, in ranked display order.
+// score	 The story's score, or the votes for a pollopt.
+// time	  Creation date of the item, in Unix Time.
+// title	 The title of the story, poll or job.
+// type	  The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+// url	 The URL of the story.
+const stories = [
+  {
+    "by" : "dhouston",
+    "descendants" : 2,
+    "id" : 0,
+    "kids" : [11, 12],
+    "score" : 6,
+    "time" : 1175714200,
+    "title" : "My YC app: Dropbox - Throw away your USB drive",
+    "type" : "story",
+    "url" : "http://www.getdropbox.com/u/2/screencast.html"
+  },
+  {
+    "by" : "probable",
+    "descendants" : 1,
+    "id" : 1,
+    "kids" : [13],
+    "score" : 2,
+    "time" : 1245744200,
+    "title" : "styled-components",
+    "type" : "story",
+    "url" : "https://www.styled-components.com/"
+  },
+];
+
 class App extends Component {
   render() {
-    const helloWorld = "HELLO WORLD"
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        <h2>Stories</h2>
+
+        {stories.map(function(item) {
+          return (
+            <div key={item.id}>
+              <a href={item.url}>{item.title}</a>
+              <p>by: {item.by} </p>
+              <p>comments: {item.descendants}</p>
+              <p>score: {item.score}</p>
+              <hr/>
+            </div>
+          )
+        })}
       </div>
     );
   }
