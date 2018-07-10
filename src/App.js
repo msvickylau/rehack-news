7d5ffc85a770039185952a27f18044d6fd9afb1e
vlaunyc <vlau.nyc@gmail.com>
2018-07-10 11:42:35 -0400

    changed Search to a class component instead of stateless component. ref DOM note so input field is focused when applicaiton renders.

--- src/App.js
+++ src/App.js
@@ -175,23 +175,38 @@ class App extends Component {
 }
 export default App;
 
+// the 'this' object references the DOM node with the ref attribute.
+// focuses on the input feild when the component mounted.
+class Search extends Component {
+    componentDidMount() {
+    if(this.input) {
+      this.input.focus();
+    }
+  }
 
-const Search = ({
-  value,
-  onChange,
-  onSubmit,
-  children
-}) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-</form>
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 
 const Table = ({ stories, onDismiss }) =>
