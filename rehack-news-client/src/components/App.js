00f271cf688dc364d4bf549aec89b500041fc4e1
vlaunyc <vlau.nyc@gmail.com>
2018-07-25 15:22:20 -0400

    moved router into App component.

--- rehack-news-client/src/components/App.js
+++ rehack-news-client/src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 import SearchContainer from './mainPage/SearchContainer'
+import Saves from './savesPage/Saves'
+import Login from './loginPage/Login'
+
+import NavBar from './common/NavBar'
+
 
 class App extends Component {
   render() {
     return (
-      <SearchContainer />
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/" component={SearchContainer}/>
+          <Route path="/saves" component={Saves}/>
+          <Route path="/login" component={Login}/>
+        </div>
+      </Router>
     )
   }
 }
--- rehack-news-client/src/index.js
+++ rehack-news-client/src/index.js
@@ -4,13 +4,9 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import App from './components/App';
-import Saves from './components/savesPage/Saves'
-import Login from './components/loginPage/Login'
-import NavBar from './components/common/NavBar'
 import { loadSaves } from './actions/saveActions'
+import App from './components/App';
 
 const store = configureStore();
 
@@ -19,15 +15,8 @@ store.dispatch(loadSaves());
 //Here we wrap <Router> component in a <Provider> component that Redux gives us access to, and pass in an instance of our store.
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <div>
-        <NavBar />
-        <Route exact path="/" component={App}/>
-        <Route path="/saves" component={Saves}/>
-        <Route path="/login" component={Login}/>
-      </div>
-    </Router>
-  </Provider> ,
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
