b74e8463a7ef46fecc580e60989ac5ba9da00cb2
vlaunyc <vlau.nyc@gmail.com>
2018-07-26 02:55:27 -0400

    add create and destroy to Save.

--- rehack-news-api/app/controllers/saves_controller.rb
+++ rehack-news-api/app/controllers/saves_controller.rb
@@ -2,8 +2,25 @@ class SavesController < ApplicationController
   def index
     @saves = Save.all
 
-    render(
-      json: @saves.all
+    render json: @saves, status: :ok
+  end
+
+  def create
+    @save = Save.new(save_params)
+    @save.save
+    render json: @save, status: :created
+  end
+
+  def destroy
+    @save = Save.where(id: params[:id]).first
+    @save.destroy
+  end
+
+  def save_params
+    params.permit(
+      :objectID,
+      :title,
+      :url
     )
   end
 end
