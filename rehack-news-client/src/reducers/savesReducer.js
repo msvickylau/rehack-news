f49ebccd00ad8ee7c6fc1e5b35f00ac681f87fde
vlaunyc <vlau.nyc@gmail.com>
2018-07-29 23:08:36 -0400

    add case create save success.

--- rehack-news-client/src/reducers/savesReducer.js
+++ rehack-news-client/src/reducers/savesReducer.js
@@ -3,8 +3,16 @@ import initialState from './initialState';
 
 export default function saveReducer(state = initialState.saves, action) {
   switch(action.type) {
+
     case types.LOAD_SAVES_SUCCESS:
       return action.saves //to return a new state.
+
+    case types.CREATE_SAVE_SUCCESS:
+      return [
+        ...state.filter(save => save.id !== action.save.id),
+        Object.assign({}, action.save)
+      ]
+
     default:
       return state;
   }
