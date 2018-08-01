c5e866c33f21d91007805f6c1caeb3ba3552baab
vlaunyc <vlau.nyc@gmail.com>
2018-08-01 15:58:45 -0400

    added redux dev tools.

--- rehack-news-client/src/store/configureStore.js
+++ rehack-news-client/src/store/configureStore.js
@@ -1,11 +1,14 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    compose(applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 }
 //createStore allows us to (1) connect our store to the rootReducer -- which wraps our other reducers. (2) Utilize the Thunk middlewear which allows us to construct our action reators in a specific way.
